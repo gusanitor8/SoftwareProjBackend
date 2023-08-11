92f37037307b96133d4181d4722a7af86ffc417c
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-11 08:57:05 -0600

    Anicam Data Update Function to db_manager
    Implemented a new feature in the db_manager to incorporate a function that enables the update of data within the 'Anicam' view

--- src/db_manager.py
+++ src/db_manager.py
@@ -92,3 +92,122 @@ def getAnicamViewSql():
 
     result = jsonifyResponse(rows, column_names)
     return result
+
+
+def updateAnicamData(**kwargs):
+    session = Session()
+    try:
+        # Verificamos si la guía existe
+        guia = session.query(Guia.Guia).filter(Guia.Guia.no_guia == kwargs.get("no_guia")).first()
+        
+        # Si la guía existe, hacemos los updates
+        if guia:
+            # Actualizamos los datos de la guia
+            guia.fecha = kwargs.get("fecha", guia.fecha)
+            
+            remitente = session.query(Remitente.Remitente).filter(Remitente.Remitente.No_guia == kwargs.get("no_guia")).first()
+            if remitente:
+                # Actualizamos los datos del remitente
+                remitente.compania = kwargs.get("compania", remitente.compania)
+                remitente.contacto = kwargs.get("contacto", remitente.contacto)
+                remitente.email = kwargs.get("email", remitente.email)
+                remitente.direccion = kwargs.get("direccion", remitente.direccion)
+                remitente.codigo_postal = kwargs.get("codigo_postal", remitente.codigo_postal)
+                remitente.ciudad = kwargs.get("ciudad", remitente.ciudad)
+                remitente.estado = kwargs.get("estado", remitente.estado)
+                remitente.pais = kwargs.get("pais", remitente.pais)
+                remitente.telefono = kwargs.get("telefono", remitente.telefono)
+
+                
+            destino = session.query(Destino.Destino).filter(Destino.Destino.No_guia == kwargs.get("no_guia")).first()
+            if destino:
+                # Actualizamos los datos del destino
+                destino.nombre = kwargs.get("nombre", destino.nombre)
+                destino.direccion = kwargs.get("direccion", destino.direccion)
+                destino.codigo_postal = kwargs.get("codigo_postal", destino.codigo_postal)
+                destino.ciudad = kwargs.get("ciudad", destino.ciudad)
+                destino.estado = kwargs.get("estado", destino.estado)
+                destino.pais = kwargs.get("pais", destino.pais)
+                destino.telefono = kwargs.get("telefono", destino.telefono)
+                
+            paquete = session.query(Paquete.Paquete).filter(Paquete.Paquete.No_guia == kwargs.get("no_guia")).first()
+            if paquete:
+                # Actualizamos los datos del paquete
+                paquete.contenido = kwargs.get("contenido", paquete.contenido)
+                paquete.piezas = kwargs.get("piezas", paquete.piezas)
+                paquete.peso_libras = kwargs.get("peso_libras", paquete.peso_libras)
+                paquete.peso_kilos = kwargs.get("peso_kilos", paquete.peso_kilos)
+                paquete.valor_declarado = kwargs.get("valor_declarado", paquete.valor_declarado)
+                paquete.news = kwargs.get("news", paquete.news)
+                paquete.linea_de_negocio = kwargs.get("linea_de_negocio", paquete.linea_de_negocio)
+                paquete.pre_alerta = kwargs.get("pre_alerta", paquete.pre_alerta)
+                paquete.guia_externa = kwargs.get("guia_externa", paquete.guia_externa)
+                paquete.precinto = kwargs.get("precinto", paquete.precinto)
+                paquete.largo = kwargs.get("largo", paquete.largo)
+                paquete.alto = kwargs.get("alto", paquete.alto)
+                paquete.ancho = kwargs.get("ancho", paquete.ancho)
+                paquete.carrier = kwargs.get("carrier", paquete.carrier)
+
+                
+        else:
+            # Si la guía no existe, hacemos los inserts
+            new_guia = Guia.Guia(
+                no_guia = kwargs.get("no_guia"),
+                fecha = kwargs.get("fecha")
+            )
+            session.add(new_guia)
+            
+            new_remitente = Remitente.Remitente(
+                No_guia = kwargs.get("no_guia"),
+                compania = kwargs.get("compania"),
+                contacto = kwargs.get("contacto"),
+                email = kwargs.get("email"),
+                direccion = kwargs.get("direccion"),
+                codigo_postal = kwargs.get("codigo_postal"),
+                ciudad = kwargs.get("ciudad"),
+                estado = kwargs.get("estado"),
+                pais = kwargs.get("pais"),
+                telefono = kwargs.get("telefono")
+                
+            )
+            session.add(new_remitente)
+            
+            new_destino = Destino.Destino(
+                No_guia = kwargs.get("no_guia"),
+                nombre = kwargs.get("nombre"),
+                direccion = kwargs.get("direccion"),
+                codigo_postal = kwargs.get("codigo_postal"),
+                ciudad = kwargs.get("ciudad"),
+                estado = kwargs.get("estado"),
+                pais = kwargs.get("pais"),
+                telefono = kwargs.get("telefono")
+            )
+            session.add(new_destino)
+            
+            new_paquete = Paquete(
+                No_guia = kwargs.get("no_guia"),
+                contenido = kwargs.get("contenido"),
+                piezas = kwargs.get("piezas"),
+                peso_libras = kwargs.get("peso_libras"),
+                peso_kilos = kwargs.get("peso_kilos"),
+                valor_declarado = kwargs.get("valor_declarado"),
+                news = kwargs.get("news"),
+                linea_de_negocio = kwargs.get("linea_de_negocio"),
+                pre_alerta = kwargs.get("pre_alerta"),
+                guia_externa = kwargs.get("guia_externa"),
+                precinto = kwargs.get("precinto"),
+                largo = kwargs.get("largo"),
+                alto = kwargs.get("alto"),
+                ancho = kwargs.get("ancho"),
+                carrier = kwargs.get("carrier")
+            )
+            session.add(new_paquete)
+            
+        session.commit()
+        return True
+
+    except Exception as e:
+        return False
+
+    finally:
+        session.close()
