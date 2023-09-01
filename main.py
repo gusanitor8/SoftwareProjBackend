08f5132e7b2fa9bd1ab181b1db2b6a5abd7c6e00
gusanitor8 <gusanitor8@gmail.com>
2023-09-01 00:11:46 -0600

    Revert "Revert "merge de implementacion de middleware de CORS""
    This reverts commit e3fa789867e291a146a9c903b3d4531def8a7c25.

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from config.database import engine, Base, Session
-from typing import List
+from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.excelDB import excelDB_router
 from routers.views import views_router
-
-#pydantic
-from dataModels.Guia import GuiaBase as GuiaModel
-
-#ORM
-from models.Guia import Guia
-from models.Cuscar import Cuscar
-from models.Destino import Destino
-from models.Facturacion import Facturacion
-from models.Oea import OEA
-from models.Paquete import Paquete
-from models.Remitente import Remitente
+from middlewares.cors_middleware import getOrigins
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 app.title = "SLI Backend API"
+
+#CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=getOrigins(),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#Error Handler
 app.add_middleware(ErrorHandler)
+
+#Routers
 app.include_router(excelDB_router)
 app.include_router(views_router)
 
+#Database
 Base.metadata.create_all(bind=engine)
 
 @app.get("/")
