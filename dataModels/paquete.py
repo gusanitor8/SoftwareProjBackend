21995aed6f0c762857091b09b4ca294531f137fc
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-16 21:32:05 -0600

    Restauracion de modelo paquete

---
+++ dataModels/paquete.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class PaqueteBase(BaseModel):
+    id_paquete: int
+    pedido_id: int
+    codigo_rastreo: str
+    contenido: str
+    descripcion: str
+    alto: float
+    ancho: float
+    largo: float
+    peso_libras: float
+    peso_volumetrico: float
+    valor_producto_dolar: float
+    unidades: int
+
+    class Config:
+        orm_mode = True
