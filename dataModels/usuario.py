9987612ab353926b5f41c1e4d1b4f1c6665bcb81
gusanitor8 <gusanitor8@gmail.com>
2023-10-17 22:00:15 -0600

    bug fix en regex de usuario en pydantic

--- dataModels/usuario.py
+++ dataModels/usuario.py
@@ -6,11 +6,11 @@ class UsuarioBase(BaseModel):
     username: constr(strip_whitespace=True, min_length=1)
     password: constr(strip_whitespace=True, min_length=1)
     nombre: constr(strip_whitespace=True, min_length=1)
-    email: constr(strip_whitespace=True, pattern="^[a-zA-Z0-9._-+]+@[a-zA-Z0-9.-+]+\\.[a-zA-Z]{2,}$")
+    email: constr(strip_whitespace=True, min_length=1)
     puesto: constr(strip_whitespace=True, min_length=1)
     estado: bool
     rol: constr(strip_whitespace=True, pattern="^(viewer|editor|admin)$") = "viewer"
     salt: constr(strip_whitespace=True, min_length=1)
 
     class Config:
-        orm_mode = True    
+        orm_mode = True
