871c5df34259128424f24bfa8cf9e5a39da74ccf
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-24 15:57:30 -0600

    Refactor usuario
    Eliminacion de atributos inutiles

--- dataModels/usuario.py
+++ dataModels/usuario.py
@@ -3,11 +3,9 @@ from pydantic import BaseModel, constr
 
 class UsuarioBase(BaseModel):
     id_usuario: int
-    username: constr(strip_whitespace=True, min_length=1)
     password: constr(strip_whitespace=True, min_length=1)
     nombre: constr(strip_whitespace=True, min_length=1)
     email: constr(strip_whitespace=True, pattern="^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
-    puesto: constr(strip_whitespace=True, min_length=1)
     estado: bool
     rol: constr(strip_whitespace=True, pattern="^(viewer|editor|admin)$") = "viewer"
     salt: constr(strip_whitespace=True, min_length=1)
--- models/usuario_table.py
+++ models/usuario_table.py
@@ -6,11 +6,9 @@ class Usuario(Base):
     __tablename__ = "usuario"
 
     id_usuario = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     nombre = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    puesto = Column(String, nullable=False)
     estado = Column(Boolean, nullable=False)
     rol = Column(String, CheckConstraint("rol in ('viewer', 'editor', 'admin')"), nullable=False)
     salt = Column(String, unique=True, nullable=False)
